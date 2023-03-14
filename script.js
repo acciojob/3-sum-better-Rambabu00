d63b6abbf649e9bf62e8c754a8dc933443c693f5
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-03-14 20:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
-  
+let min=MAX_VALUE;
+  for (let index = 0; index <arr.length-2 ; index++) {
+	  let sum=0;
+  	for(let i=index;i<=index+2;i++){
+		sum+=arr[i];
+	}
+	  if(sum<=min){
+		  min=sum;
+	  }
+  }
+	return min;
 }
 
 module.exports = threeSum;
