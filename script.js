0124d827eda245d3899e205053dc409679fb9e76
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-03-23 15:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for(let i=0;i<arr.length;i++){
 	for(let j=i+1;j<arr.length;j++){
 		let sum=0;
 		for(let k=j+1;k<arr.length;k++){
-			if((target-closestSum)>(target-(arr[i]+arr[j]+arr[k]))){
+			if(Math.abs(target-closestSum)>Math.abs(target-(arr[i]+arr[j]+arr[k]))){
 				closestSum=arr[i]+arr[j]+arr[k];
 			}
 		}
