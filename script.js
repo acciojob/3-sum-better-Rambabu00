e4cb1d12ff2f7afa6df1e01520541adef338fb55
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-03-23 15:23:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function threeSum(arr, target) {
 // write your code he
+
+    let closestSum = Integer.MAX_VALUE;
 for(let i=0;i<arr.length;i++){
 	for(let j=i+1;j<arr.length;j++){
 		let sum=0;
 		for(let k=j+1;k<arr.length;k++){
-			sum+=arr[i]+arr[j]+arr[k];
-			if(sum==target){
-				return sum;
-			}
-			else if(sum+1==target){
-				return sum;
+			if((target-closestSum)>(target-(arr[i]+arr[j]+arr[k]))){
+				closestSum=arr[i]+arr[j]+arr[k];
 			}
 		}
 		
